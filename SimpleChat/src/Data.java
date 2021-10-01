e77ba185225648d89006a70992780da30faaaf86
Kamrujjaman <kamrujjaman05081@gmail.com>
2021-10-02 03:18:27 +0600

    All AddedBut missing org

---
+++ SimpleChat/src/Data.java
@@ -0,0 +1,12 @@
+
+import java.io.Serializable;
+
+public class Data implements Serializable,Cloneable{
+    
+    public String message;           
+    
+    public Object clone()throws CloneNotSupportedException{  
+        return super.clone();
+    }  
+}
+  
